53af627d47068b77380ac080cc57b340682bf257
saurabhchoudhary45 <saurabhchoudhary32145@gmail.com>
2024-02-26 13:43:45 +0530

    26-02-2024(1:43pm)

---
+++ public/Bharatclock.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Bharatclock() {
+  return (
+    <div class="card" aline="center">
+    <div class="card-body">
+      <h1 class="Bharatclock">Card title</h1>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      
+    </div>
+  
+
+
+    </div>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Aboutus from './Aboutus.jsx'
+
+export default function App() {
+  return (
+  <div className='row'>
+  
+   <div class="card col"  >
+  <img src="https://seeklogo.com/images/C/c-programming-language-logo-9B32D017B1-seeklogo.com.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">C-language</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">explore more</a>
+  </div>
+  
+</div>
+
+<div class="card col"  >
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">C++</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">explore more</a>
+  </div>
+  </div>
+  <div class="card col"  >
+  <img src="https://e7.pngegg.com/pngimages/520/669/png-clipart-c-logo-c-programming-language-computer-icons-computer-programming-programming-miscellaneous-blue.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">C#</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">explore more</a>
+  </div>
+  </div>
+  
+  
+<div class="card col"  >
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">python</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">explore more</a>
+  </div>
+</div>
+<div class="card col"  >
+  <img src="https://www.devopsschool.com/blog/wp-content/uploads/2022/03/java_logo_icon_168609.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">java</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">explore more</a>
+  </div>
+  </div>
+</div>  
+
+  )
+}
